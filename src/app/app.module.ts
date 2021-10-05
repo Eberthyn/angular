75ddc1ab32c560165f824605580b697de826e061
Eberthyn Marques <eberthyn.marques@saude.gov>
2021-10-05 18:03:44 -0300

    Integração Extrato e Formulário com a API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { HttpClientModule } from '@angular/common/http';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt' }, {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'}],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
